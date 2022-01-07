4878363bafaf7f5ead3262e2fab552f89eb13f1e
Will Hansen <will.r.s.hansen@gmail.com>
2022-01-07 00:02:42 -0800

    A little polish

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,62 @@
 extern crate line_drawing;
+extern crate std;
 extern crate termion;
 
 use std::io::{stdin, stdout, Write};
-use termion::event::{Event, Key, MouseEvent};
+use termion::event::{Event, Key, MouseButton, MouseEvent};
 use termion::input::{MouseTerminal, TermRead};
 use termion::raw::IntoRawMode;
+
+struct Game {
+    grid: Vec<Vec<char>>,
+}
+impl Game {
+    fn draw_line<W: std::io::Write>(
+        &mut self,
+        stdout: &mut MouseTerminal<W>,
+        pos0: (u16, u16),
+        pos1: (u16, u16),
+        character: char,
+    ) {
+        for (x1, y1) in line_drawing::Bresenham::new(
+            (pos0.0 as i32, pos0.1 as i32),
+            (pos1.0 as i32, pos1.1 as i32),
+        ) {
+            write!(
+                stdout,
+                "{}{}",
+                termion::cursor::Goto(x1 as u16, y1 as u16),
+                character
+            )
+            .unwrap();
+        }
+    }
+
+    fn draw_point<W: std::io::Write>(
+        &mut self,
+        stdout: &mut MouseTerminal<W>,
+        pos: (u16, u16),
+        character: char,
+    ) {
+        write!(
+            stdout,
+            "{}{}",
+            termion::cursor::Goto(pos.0, pos.1),
+            character
+        )
+        .unwrap();
+    }
+}
 
 fn main() {
     let stdin = stdin();
     let mut stdout = MouseTerminal::from(stdout().into_raw_mode().unwrap());
     let mut prev_mouse_pos = (1, 1);
+    let (width, height) = termion::terminal_size().unwrap();
+    let selected_char = '█';
+    let mut game = Game {
+        grid: vec![vec![' '; width as usize]; height as usize],
+    };
 
     write!(
         stdout,
@@ -25,16 +72,22 @@ fn main() {
         match evt {
             Event::Key(Key::Char('q')) => break,
             Event::Key(Key::Char('c')) => {
-                write!(stdout, "{}{}", termion::clear::All, termion::cursor::Goto(1, 1)).unwrap();
+                write!(
+                    stdout,
+                    "{}{}",
+                    termion::clear::All,
+                    termion::cursor::Goto(1, 1)
+                )
+                .unwrap();
             }
             Event::Mouse(me) => match me {
-                MouseEvent::Press(_, x, y) => {
-                    draw_point(&mut stdout, (x, y));
+                MouseEvent::Press(MouseButton::Left, x, y) => {
+                    game.draw_point(&mut stdout, (x, y), selected_char);
                     write!(stdout, "{}", termion::cursor::Goto(1, 1)).unwrap();
                     prev_mouse_pos = (x, y);
                 }
                 MouseEvent::Hold(x, y) => {
-                    draw_line(&mut stdout, prev_mouse_pos, (x, y));
+                    game.draw_line(&mut stdout, prev_mouse_pos, (x, y), selected_char);
                     write!(stdout, "{}", termion::cursor::Goto(1, 1)).unwrap();
                     prev_mouse_pos = (x, y);
                 }
@@ -43,18 +96,5 @@ fn main() {
             _ => {}
         }
         stdout.flush().unwrap();
-    }
-}
-
-fn draw_point<W: std::io::Write>(stdout: &mut MouseTerminal<W>, pos: (u16, u16)) {
-    write!(stdout, "{}x", termion::cursor::Goto(pos.0, pos.1)).unwrap();
-}
-
-fn draw_line<W: std::io::Write>(stdout: &mut MouseTerminal<W>, pos0: (u16, u16), pos1: (u16, u16)) {
-    for (x1, y1) in line_drawing::Bresenham::new(
-        (pos0.0 as i32, pos0.1 as i32),
-        (pos1.0 as i32, pos1.1 as i32),
-    ) {
-        write!(stdout, "{}x", termion::cursor::Goto(x1 as u16, y1 as u16)).unwrap();
     }
 }
